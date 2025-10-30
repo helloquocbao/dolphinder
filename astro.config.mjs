4c86057d2639d17ff7f3a616376916d25ba32e51
LHQBao <bao@sadec.co>
2025-10-30 16:17:46 +0700

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +12,5 @@ export default defineConfig({
 
   integrations: [react()],
   output: "server",
+  adapter: vercel(),
 });
